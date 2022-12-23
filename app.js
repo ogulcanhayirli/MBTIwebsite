58b8646dae01fdb039b3cc048961e626e8054315
Ogulcan Hayırlı <ogulcanhayirli@Ogulcans-MBP.home>
2022-12-23 17:45:05 +0300

    Only Username and Password

--- app.js
+++ app.js
@@ -3,181 +3,275 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const port = 3000;
-const multer = require("multer")
-const path = require("path")
-const fs = require("fs")
-const lodash =require("lodash")
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+require("dotenv").config();
+
+const lodash = require("lodash");
 const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    cb(null, "public/images")
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
   },
 
-  filename: (req, file, cb) =>{
-    console.log(file)
-    cb(null, Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({storage: storage})
-
-
-const homeStartingContent = "Burada eklediğin kişiler ve tipleri gözükecektir yukaridaki ekle butonundan yeni kişiler ekleyebilir ya da ara butonundan istediğin kişiyi görebilirsin ";
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
+const homeStartingContent =
+  "Burada eklediğin kişiler ve tipleri gözükecektir yukaridaki ekle butonundan yeni kişiler ekleyebilir ya da ara butonundan istediğin kişiyi görebilirsin ";
 
 const app = express();
-const _ = require("lodash");
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const { isMainThread } = require("worker_threads");
 const { userInfo } = require("os");
 
-// mongodb://localhost:27017/MBTIDB
-// mongodb+srv://admin:test123@todolist.ac9ps0o.mongodb.net/todolistDB
+
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb+srv://admin:test123@todolist.ac9ps0o.mongodb.net/todolistDB", {
+mongoose.connect(process.env.DATABASE_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 function escapeRegex(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
-app.use(express.static("public"));
+}
 
 const personSchema = new mongoose.Schema({
   name: {
     required: true,
     type: String,
   },
-  type : {
+  type: {
     required: true,
-    type: String
+    type: String,
   },
-  reviews: [{review:String}],
-  img: String
+  reviews: [{ review: String, author: String }],
+  img: String,
 });
 
 const Person = mongoose.model("Person", personSchema);
+app.get("/", function (req, res) {
+  res.render("login");
+});
 
+app.post("/", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-
-app.get("/", function(req, res){
-  if(req.query.search){
-    const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    Person.find({name:regex}, function(err, foundPosts){
-      var noMatch;
-      if(foundPosts.length < 1){
-        var noMatch = "Böyle biri sistemde yok oluşturmayı deneyebilirsin"
-        startingContent = undefined;
-      }
-      res.render("home", {
-        startingContent: homeStartingContent,
-        posts: foundPosts,
-        noMatch: noMatch
-      })}); 
-  }else{
-    var noMatch;
-  Person.find({}, function(err, foundPosts){
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: foundPosts,
-      noMatch: noMatch
-      }); 
-  })};
-})
-
-
-app.get("/char/:charID", function(req,res){
-  const requestedID = req.params.charID;
-
-  Person.findOne({_id: requestedID}, function(err, char){
-    res.render("char", {char:char});
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/home");
+      });
+    }
   });
 });
 
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-})
-
-app.get("/compose/update", function(req, res){
-  const requestedID = req.query.updatedChar
-  Person.findOne({_id: requestedID}, function(err, char){
-    res.render("composeupdate",{char:char});
-  });
-})
-
-app.post("/compose/update/:charID", upload.single("image"),function(req,res){
-  Person.findOne({_id:req.body.updatedChar}, function(err, foundChar){
-    if(req.body.charName !== undefined){
-      foundChar.name = lodash.capitalize(req.body.charName)
-    }
-    if(req.body.charType !== undefined){
-      foundChar.type = req.body.charType
-    }
-    if(req.file && req.file.originalname){
-      if (foundChar.img !== "/images/default.jpeg"){
-      console.log("calisti")
-      let filePath = "./public" +foundChar.img
-      fs.unlink(filePath,function(err){
-        if(err){
-          console.log(err)
+app.get("/home", function (req, res) {
+  if (req.isAuthenticated()) {
+    if (req.query.search) {
+      const regex = new RegExp(escapeRegex(req.query.search), "gi");
+      Person.find({ name: regex }, function (err, foundPosts) {
+        var noMatch;
+        if (foundPosts.length < 1) {
+          var noMatch = "Böyle biri sistemde yok oluşturmayı deneyebilirsin";
+          startingContent = undefined;
         }
-        
-      })}
-      foundChar.img = req.file.path.replace("public","")
-
-      }
-    foundChar.save(function(err){
-      if(!err){
-        res.redirect("/char/" + req.body.updatedChar)
-      }
-    })
-
-  })
+        res.render("home", {
+          startingContent: homeStartingContent,
+          posts: foundPosts,
+          noMatch: noMatch,
+        });
+      });
+    } else {
+      var noMatch;
+      Person.find({}, function (err, foundPosts) {
+        res.render("home", {
+          startingContent: homeStartingContent,
+          posts: foundPosts,
+          noMatch: noMatch,
+        });
+      });
+    }
+  } else {
+    res.redirect("/");
+  }
 });
 
+app.get("/char/:charID", function (req, res) {
+  if (req.isAuthenticated()) {
+    const requestedID = req.params.charID;
 
+    Person.findOne({ _id: requestedID }, function (err, char) {
+      res.render("char", { char: char });
+    });
+  } else {
+    res.redirect("/");
+  }
+});
 
-app.post("/compose", upload.single("image"),function(req,res){
+app.get("/compose", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("compose");
+  } else {
+    res.redirect("/");
+  }
+});
 
-  const newPost = new Person({
-    name: lodash.capitalize(req.body.charName),
-    type : req.body.charType,
-    reviews: [],
-    img: "/images/default.jpeg"
-  });
-  if(req.file && req.file.originalname) newPost.img = req.file.path.replace("public","");
-  newPost.save(function(err){
-    if(!err){
+app.get("/compose/update", function (req, res) {
+  if (req.isAuthenticated()) {
+    const requestedID = req.query.updatedChar;
+    Person.findOne({ _id: requestedID }, function (err, char) {
+      res.render("composeupdate", { char: char });
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post(
+  "/compose/update/:charID",
+  upload.single("image"),
+  function (req, res) {
+    if (req.isAuthenticated()) {
+      Person.findOne({ _id: req.body.updatedChar }, function (err, foundChar) {
+        if (req.body.charName !== undefined) {
+          foundChar.name = lodash.capitalize(req.body.charName);
+        }
+        if (req.body.charType !== undefined) {
+          foundChar.type = req.body.charType;
+        }
+        if (req.file && req.file.originalname) {
+          if (foundChar.img !== "/images/default.jpeg") {
+            let filePath = "./public" + foundChar.img;
+            fs.unlink(filePath, function (err) {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+          foundChar.img = req.file.path.replace("public", "");
+        }
+        foundChar.save(function (err) {
+          if (!err) {
+            res.redirect("/char/" + req.body.updatedChar);
+          }
+        });
+      });
+    } else {
       res.redirect("/");
     }
-  });
-})
+  }
+);
 
-app.post("/char/delete/:charID", function(req,res){
-  Person.deleteOne({_id:req.body.deletedChar}, function(err){
-    res.redirect("/")
-  })
+app.post("/compose", upload.single("image"), function (req, res) {
+  if (req.isAuthenticated()) {
+    const newPost = new Person({
+      name: lodash.capitalize(req.body.charName),
+      type: req.body.charType,
+      reviews: [],
+      img: "/images/default.jpeg",
+    });
+    if (req.file && req.file.originalname)
+      newPost.img = req.file.path.replace("public", "");
+    newPost.save(function (err) {
+      if (!err) {
+        res.redirect("/home");
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
-app.post("/char/addcomment/:charID", function(req,res){
-  Person.updateOne(
-    {_id:req.body.commentedChar},
-    { $push: { reviews: {review:req.body.addedComment} } }, 
-    {new:true},
-    function(err){
-      if(!err){
-        res.redirect("/char/" + req.body.commentedChar)
-      } else{
-        console.log(err)
+app.post("/char/delete/:charID", function (req, res) {
+  if (req.isAuthenticated()) {
+    if (req.body.deletedImg !== "/images/default.jpeg") {
+      let filePath = "./public" + req.body.deletedImg;
+      fs.unlink(filePath, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+    Person.deleteOne({ _id: req.body.deletedChar }, function (err) {
+      res.redirect("/home");
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post("/char/addcomment/:charID", function (req, res) {
+  if (req.isAuthenticated()) {
+    Person.updateOne(
+      { _id: req.body.commentedChar },
+      {
+        $push: {
+          reviews: {
+            review: req.body.addedComment,
+            author: lodash.capitalize(req.user.username),
+          },
+        },
+      },
+      { new: true },
+      function (err) {
+        if (!err) {
+          res.redirect("/char/" + req.body.commentedChar);
+        } else {
+          console.log("bu error");
+          console.log(err);
+        }
       }
-    })
-})
+    );
+  } else {
+    res.redirect("/");
+  }
+});
 
 
-app.listen(process.env.PORT || port, function() {
+
+
+app.listen(process.env.PORT || port, function () {
   console.log("Server started on port 3000");
 });
